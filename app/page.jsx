cde60ca1a5023245f3112e31244b27f99fe8f61f
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-03-15 03:06:34 +0330

    navigation and route related hooks in client comps

--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,6 @@ const HomePage = () => {
     <div className="text-2xl">
       <h1 className="text-2xl">welcome</h1>
       <Link href="properties">Go to properties</Link>
-      {/* <Link href="properties?name=felanProperty">Go to properties</Link>
-      <Link
-        href={{
-          pathname: "properties",
-          query: { name: "felanProperty" },
-        }}
-      >
-        Go to properties
-      </Link> */}
     </div>
   );
 };
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import {
+  useParams,
+  useRouter,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  console.log(router);
+
+  const params = useParams();
+  console.log(params);
+
+  const searchParams = useSearchParams();
+  console.log(searchParams);
+  console.log(searchParams.get("name"));
+  // http://localhost:3000/properties/14?name=ali
+
+  const pathName = usePathname();
+  console.log(pathName);
+
+  return (
+    <div>
+      <button onClick={() => router.replace("/")}> go to home</button>
+    </div>
+  );
 };
 
 export default PropertyPage;
