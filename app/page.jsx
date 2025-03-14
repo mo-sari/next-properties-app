2569083ca772ad74d850551afc88519ae9cd846f
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-03-15 02:40:33 +0330

    basic routing and route changing stuff

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 const HomePage = () => {
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <div className="text-2xl">
+      <h1 className="text-2xl">welcome</h1>
+      <Link href="properties">Go to properties</Link>
+      {/* <Link href="properties?name=felanProperty">Go to properties</Link>
+      <Link
+        href={{
+          pathname: "properties",
+          query: { name: "felanProperty" },
+        }}
+      >
+        Go to properties
+      </Link> */}
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ app/properties/add/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddPropertyPage() {
+  return <div>AddPropertyPage</div>;
+}
+
+export default AddPropertyPage;
