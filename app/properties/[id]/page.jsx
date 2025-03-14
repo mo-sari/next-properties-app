48c18707bdf4b03cf4a63c2f4fd2a1e7932445eb
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-03-15 03:08:56 +0330

    the same as previous commit, in server comps

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,29 +1,8 @@
-"use client";
-import {
-  useParams,
-  useRouter,
-  useSearchParams,
-  usePathname,
-} from "next/navigation";
-
-const PropertyPage = () => {
-  const router = useRouter();
-  console.log(router);
-
-  const params = useParams();
-  console.log(params);
-
-  const searchParams = useSearchParams();
-  console.log(searchParams);
-  console.log(searchParams.get("name"));
-  // http://localhost:3000/properties/14?name=ali
-
-  const pathName = usePathname();
-  console.log(pathName);
-
+const PropertyPage = ({ params, searchParams }) => {
+  console.log(params, searchParams.name);
   return (
     <div>
-      <button onClick={() => router.replace("/")}> go to home</button>
+      <button> go to home</button>
     </div>
   );
 };
