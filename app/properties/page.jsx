efe16e4ab9735c39ac2c8737f5e04397c9dc1e6d
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-03-15 16:47:36 +0330

    properties and cards

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
+import properties from "@/properties.json";
 
-const Properties = () => {
-  return <div>Properties</div>;
+const PropertiesPage = async () => {
+  console.log(properties);
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h1 className="text-2xl mb-4">Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property, index) => (
+                <PropertyCard property={property} key={index} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default Properties;
+export default PropertiesPage;
